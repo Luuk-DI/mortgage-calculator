bd4326e495c9ac83076f26a9306247cd428c5461
Luuk <77783220+Luuk-DI@users.noreply.github.com>
2023-12-03 20:10:24 +0100

    Improve linting config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -32,19 +32,18 @@ module.exports = {
   },
   rules: {
     ...customized.rules,
-    '@stylistic/brace-style': ['warn', '1tbs'],
+    '@stylistic/arrow-parens': ['warn', 'as-needed'],
+    '@stylistic/brace-style': ['warn', '1tbs', { allowSingleLine: true }],
     '@stylistic/member-delimiter-style': ['warn', { multiline: { delimiter: 'semi', requireLast: true } }],
+    '@stylistic/padded-blocks': 'off', // Currently no good configuration is possible.
 
-    'no-trailing-spaces': 'warn',
-    'quotes': ['warn', 'single'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'semi': ['error', 'always'],
+
     'vue/component-name-in-template-casing': ['error', 'PascalCase', {
       registeredComponentsOnly: true,
       ignores: [],
     }],
-
     'vue/define-emits-declaration': 'error',
     'vue/define-macros-order': 'warn',
     'vue/define-props-declaration': 'error',
@@ -67,4 +66,13 @@ module.exports = {
     'vue/prefer-import-from-vue': 'error',
     'vue/prefer-true-attribute-shorthand': 'warn',
   },
+  ignorePatterns: [
+    '/*.json',
+    '/node_modules/*',
+    '/.DS_Store',
+    '/dist/*',
+    '/dist-ssr/*',
+    '/*.local',
+    '/junit.xml',
+  ],
 };
